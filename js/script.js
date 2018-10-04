a03b70fe28a1ab8a7521c98578a875e4474a9c9c
Maksim Sianko <senkomaksim@gmail.com>
2018-10-03 23:25:43 -0700

    JS

--- js/script.js
+++ js/script.js
@@ -5,22 +5,22 @@ var departureDate = searchForm.querySelector("[name=departure-date]");
 var adults = searchForm.querySelector("[name=adult]");
 var children = searchForm.querySelector("[name=children]");
 
- findButton.addEventListener("click",function(evt){
-     evt.preventDefault();
-     searchForm.classList.toggle("search-form-show");
-     arriveDate.focus();
- });
+findButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    searchForm.classList.toggle("search-form-show");
+    arriveDate.focus();
+});
 
-searchForm.addEventListener("submit", function(evt){ 
+searchForm.addEventListener("submit", function (evt) {
     if (!arriveDate.value || !departureDate.value || !adults.value || !children.value) {
         evt.preventDefault();
         searchForm.classList.remove("search-form-error");
-        setTimeout(function (){
+        setTimeout(function () {
             searchForm.classList.add("search-form-error");
-        },4);
+        }, 4);
     } else {
+
         searchForm.classList.remove("search-form-error");
     }
-    
-});
 
+});
